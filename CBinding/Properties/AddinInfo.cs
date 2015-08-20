88f7a8503322747d3d5f174eb7ce42a95ce08367
Michael Hutchinson <m.j.hutchinson@gmail.com>
2015-08-20 17:17:58 -0400

    Bump version to override built-in C addin

--- CBinding/Properties/AddinInfo.cs
+++ CBinding/Properties/AddinInfo.cs
@@ -5,7 +5,7 @@ using Mono.Addins.Description;
 
 [assembly:Addin ("CBinding", 
 	Namespace = "MonoDevelop",
-	Version = MonoDevelop.BuildInfo.Version,
+	Version = "6.1",
 	Category = "Language bindings")]
 
 [assembly:AddinName ("C/C++ Language Binding")]
